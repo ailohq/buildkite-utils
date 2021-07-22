57dc77973c2696201680667e81d4704cb5e7bdde
micheal <micheal.hill@ailo.io>
2021-07-22 13:53:20 +1000

    feat!: flatten/deduplicate pipelines that contain dependency stacks

--- src/operators.ts
+++ src/operators.ts
@@ -22,8 +22,48 @@ export function buildPipeline(
   console.log(
     JSON.stringify({
       env,
-      // todo build pipeline properly
-      steps: pipeline,
+      steps: deduplicatePipeline(pipeline.flatMap((step) => step.derivedSteps)),
     }),
   );
 }
+
+function deduplicatePipeline(steps: PipelineStep[]) {
+  function stepHasMatchingKey(key?: string) {
+    return function (step: PipelineStep) {
+      return ("key" in step) && step.key === key;
+    };
+  }
+
+  function isDuplicateStep(step: PipelineStep, agg: PipelineStep[]) {
+    return ("key" in step) &&
+      agg.find(stepHasMatchingKey(step.key)) !== undefined;
+  }
+
+  function aggregator(
+    agg: PipelineStep[],
+    [head, ...tail]: PipelineStep[],
+  ): PipelineStep[] {
+    const isNewStep = !isDuplicateStep(head, agg);
+    if (isNewStep) {
+      console.warn(`found new step ${head.key}`);
+    }
+
+    const nextAgg = [
+      ...agg,
+      ...(isNewStep ? [head] : []),
+    ];
+
+    if (tail.length > 0) {
+      return aggregator(nextAgg, tail);
+    } else {
+      return nextAgg;
+    }
+  }
+
+  return aggregator([], steps);
+}
+
+type PipelineStep = {
+  key?: string;
+  "if"?: string;
+};
